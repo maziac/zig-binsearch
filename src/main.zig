a39edd47be4a9c41bf94c66829e26e3c25568169
maziac <thomas.busse@gmx.net>
2023-04-04 22:38:52 +0200

    Reading 1rst argument.

--- src/main.zig
+++ src/main.zig
@@ -4,4 +4,20 @@ pub fn main() void {
     //var a: i32 = 13;
     std.debug.print("Hello, {s}!\n", .{"World"});
     //std.debug.print("the value a = {}\n", "gg");
+
+    var args = try std.process.argsWithAllocator(std.heap.page_allocator);
+    defer args.deinit();
+
+    const executable_name = args.next();
+    //  orelse {
+    //     try .print.process(error.NoExecutableName, Error{
+    //         .option = "",
+    //         .kind = .missing_executable_name,
+    //     });
+
+    //     // we do not assume any more arguments appear here anyways...
+    //     return error.NoExecutableName;
+    // };
+
+    std.debug.print("executable_name={any}\n", .{executable_name});
 }
